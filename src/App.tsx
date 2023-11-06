ca2075498e28bd742617bfbee5c6e6d1d7c12967
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-11-06 17:17:27 -0300

    Refactored code to improve performance and readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-
 import './assets/styles/global.css';
 
 function App() {
-  return (
-    <h1>Olá mundo</h1>
-  );
+	return <h1>Olá mundo</h1>;
 }
 
 export default App;
