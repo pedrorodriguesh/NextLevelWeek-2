479ab40f82be8128024eff6b975507e5d2d0d312
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-11-07 16:43:23 -0300

    Add react-router-dom and @remix-run/router dependencies

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,8 @@
+
+function TeacherForm() {
+	return (
+		<h1>Teachers Form</h1>
+	);
+}
+
+export default TeacherForm;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,8 @@
+
+function TeacherList() {
+	return (
+		<h1>Teachers</h1>
+	);
+}
+
+export default TeacherList;
