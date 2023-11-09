51005f49278eb9ce9d497400972dcfaeb3103958
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-11-09 16:12:50 -0300

    Refactor TeacherList page to use PageHeader component

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,28 +4,12 @@ import { Link } from 'react-router-dom';
 import backIcon from '../../assets/images/icons/back.svg';
 import logoImg from '../../assets/images/logo.svg';
 
+import PageHeader from '../../components/PageHeader';
 import './styles.css';
 
 function TeacherList() {
 	return (
-
-		<div id='page-teacher-list' className='container'>
-			<header className='page-header'>
-
-				<div className='top-bar-container'>
-					<Link to='/'>
-						<img src={backIcon} alt='voltar' />
-					</Link>
-
-					<img src={logoImg} alt='logo-proffy' />
-				</div>
-
-				<div className='header-content'>
-					<strong>Estes são os proffys disponíveis.</strong>
-				</div>
-
-			</header>
-		</div>
+		<PageHeader />
 	);
 }
 
