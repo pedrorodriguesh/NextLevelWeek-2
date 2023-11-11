f78cea99f9765ff2abff7f492579c6f0a00d5d6b
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-11-11 15:20:18 -0300

    Add knex and sqlite3 dependencies to package.json

---
+++ server/src/database/connection.ts
@@ -0,0 +1,14 @@
+// Conexão com banco de dados => SQLite3
+
+import knex from 'knex'
+import path from 'path'
+
+const db = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite')
+    },
+    useNullAsDefault: true,
+})
+
+export default db
