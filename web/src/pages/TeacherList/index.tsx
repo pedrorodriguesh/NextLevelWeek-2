4c5589b0b5db1eeea74070db3e6954c3fd5e3e68
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-11-12 12:23:10 -0300

    Add subject select and adjust styles

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 import './styles.css';
 
 function TeacherList() {
@@ -8,7 +9,24 @@ function TeacherList() {
 		<div id='page-teacher-list' className='container'>
 			<PageHeader title='Estes são os proffys disponíveis.'>
 				<form id='search-teachers'>
-					<Input name='subject' label='Matéria'/>
+				<Select 
+						name='subject' 
+						label='Matéria'
+						options={[
+							{ value: 'Biologia', label: 'Biologia' },
+							{ value: 'Ciências', label: 'Ciências' },
+							{ value: 'Física', label: 'Física' },
+							{ value: 'Geografia', label: 'Geografia' },
+							{ value: 'História', label: 'História' },
+							{ value: 'Matemática', label: 'Matemática' },
+							{ value: 'Português', label: 'Português' },
+							{ value: 'Química', label: 'Química' },
+							{ value: 'Inglês', label: 'Inglês' },
+							{ value: 'Programação', label: 'Programação'},
+							{ value: 'Direito', label: 'Direito'},
+						]}
+					/>
+
 					<Input name='week_day' label='Dia da semana'/>
 					<Input name='time' label='Hora' type='time'/>
 				</form>
