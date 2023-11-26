48085968ae276c240c99047094a63cbcb2b6496a
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2023-11-25 22:03:36 -0300

    comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,8 @@ const app = express()
 const PORT = 3030;
 
 app.use(cors());
-app.use(express.json());
-app.use(routes)
+app.use(express.json()); // Para o express entender o formato json.
+app.use(routes) // Para o express entender as rotas.
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
